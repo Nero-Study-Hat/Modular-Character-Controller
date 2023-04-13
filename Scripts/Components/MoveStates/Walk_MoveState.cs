be31a965683185c480e5c4c26858be3fcb177f02
Nero-Study-Hat <85171551+Nero-Study-Hat@users.noreply.github.com>
2023-04-13 18:52:57 -0400

    Updated Logging - Walk State
    The current implementation is very hacky and no good long term but will work for now.

--- Scripts/Components/MoveStates/Walk_MoveState.cs
+++ Scripts/Components/MoveStates/Walk_MoveState.cs
@@ -22,6 +22,9 @@ public partial class Walk_MoveState : Base_MoveState
 
     public override void _Ready()
     {
+        // newLogFile();
+        new_LogFile();
+
         for (int index = 0; index < this.GetChildCount(); index++)
         {
             if (this.GetChild(index) is IMoveVelocity)
@@ -43,11 +46,14 @@ public partial class Walk_MoveState : Base_MoveState
 
     public override void Process(CharacterBody2D entity)
     {
-        moveState_Logging(entity);
+        // moveState_Logging(entity);
+        update_LogFile(entity);
     }
 
     public override void PhysicsProcess(CharacterBody2D entity)
     {
+        // Direction is working but entity.Velocity is never updating.
+
         direction = getDirection.GetDirection();
         moveVelocity.SetDirection(direction.X, direction.Y);
         moveVelocity.SetVelocity(entity.Velocity.X, entity.Velocity.Y);
@@ -60,12 +66,36 @@ public partial class Walk_MoveState : Base_MoveState
     }
 
 
-    private void moveState_Logging(CharacterBody2D entity)
+// These two functions are currently useless as I am unable to append a new line at the end of the log file.
+    // private void newLogFile()
+    // {
+    //     using var moveState_LogFile = FileAccess.Open("user://custom_logs/walkState_log.txt", FileAccess.ModeFlags.Write);
+    // }
+
+    // private void moveState_Logging(CharacterBody2D entity)
+    // {
+    //     string entity_velocity = GD.VarToStr(entity.Velocity);
+    //     string entity_direction = GD.VarToStr(direction);
+
+    //     using var moveState_LogFile = FileAccess.Open("user://custom_logs/walkState_log.txt", FileAccess.ModeFlags.ReadWrite);
+    //     moveState_LogFile.StoreString("\n");
+    //     moveState_LogFile.StoreString("Velocity: " + entity_direction + " Direction: " + entity_direction);
+    // }
+
+// This works but I copying and rewriting the entire file every frame which is a horrible idea.
+    public void new_LogFile()
+    {
+        using var file = FileAccess.Open("user://custom_logs/log_file.txt", FileAccess.ModeFlags.Write);
+        file.StoreString("Hello world.");
+    }
+
+    public void update_LogFile(CharacterBody2D entity)
     {
         string entity_velocity = GD.VarToStr(entity.Velocity);
         string entity_direction = GD.VarToStr(direction);
 
-        using var moveState_LogFile = FileAccess.Open("user://custom_logs/velocityComponent_log.txt", FileAccess.ModeFlags.Write);
-        moveState_LogFile.StoreLine("Velocity: " + entity_direction + " Direction: " + entity_direction);
+        using var file = FileAccess.Open("user://custom_logs/log_file.txt", FileAccess.ModeFlags.ReadWrite);
+        string content = file.GetAsText() + "\n" + "Velocity: " + entity_velocity + " Direction: " + entity_direction;
+        file.StoreString(content);
     }
 }
