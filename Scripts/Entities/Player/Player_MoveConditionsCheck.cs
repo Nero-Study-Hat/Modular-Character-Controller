8d515e2edd75e8f99b30b6c401dfd769659fdbe4
Nero-Study-Hat <85171551+Nero-Study-Hat@users.noreply.github.com>
2023-04-21 14:46:58 -0400

    Working Switching + Idle Spawn

--- Scripts/Entities/Player/Player_MoveConditionsCheck.cs
+++ Scripts/Entities/Player/Player_MoveConditionsCheck.cs
@@ -7,6 +7,8 @@ public partial class Player_MoveConditionsCheck : BaseConditionsCheck
     CharacterBody2D player = new CharacterBody2D();
     Movement_StateMachine movement_StateMachine = new Movement_StateMachine();
 
+    MoveStateFactory moveStateFactory;
+
     MoveStateFactory.MoveStates enumVal_CurrentState;
     Dictionary<MoveStateFactory.MoveStates, Base_MoveState> entityStateDict = new Dictionary<MoveStateFactory.MoveStates, Base_MoveState>();
     Dictionary<MoveStateFactory.MoveStates, Action> currentConditionsDict = new Dictionary<MoveStateFactory.MoveStates, Action>();
@@ -18,20 +20,22 @@ public partial class Player_MoveConditionsCheck : BaseConditionsCheck
 
     private void SetAllStateChecks()
     {
-        allConditionsDict.Add(MoveStateFactory.MoveStates.BIdle_MoveState, BIdle_Check);
-        allConditionsDict.Add(MoveStateFactory.MoveStates.BNormal_MoveState, BNormal_Check);
+        allConditionsDict.Add(MoveStateFactory.MoveStates.BIdle_MoveState, BIdle_CheckConditions);
+        allConditionsDict.Add(MoveStateFactory.MoveStates.BNormal_MoveState, BNormal_CheckConditions);
     }
 
     private void SetInitialStateChecks()
     {
-        currentConditionsDict.Add(MoveStateFactory.MoveStates.BIdle_MoveState, BIdle_Check);
-        currentConditionsDict.Add(MoveStateFactory.MoveStates.BNormal_MoveState, BNormal_Check);
+        // currentConditionsDict.Add(MoveStateFactory.MoveStates.BIdle_MoveState, BIdle_CheckConditions);
+        currentConditionsDict.Add(MoveStateFactory.MoveStates.BNormal_MoveState, BNormal_CheckConditions);
     }
 
     public override void Initialize(CharacterBody2D EntityRef, Movement_StateMachine MoveStatesManager)
     {
         player = EntityRef;
         movement_StateMachine = MoveStatesManager;
+
+        moveStateFactory = new MoveStateFactory(movement_StateMachine);
         
         entityStateDict = movement_StateMachine.entityMoveStatesDict;
         SetAllStateChecks();
@@ -43,39 +47,105 @@ public partial class Player_MoveConditionsCheck : BaseConditionsCheck
         currentConditionsDict[movement_StateMachine.EnumVal_CurrentState].Invoke();
     }
 
+// --
 
-    private void BIdle_Check()
+    private void BIdle_CheckConditions()
     {
-        var _BNormal_EnterCheck = BIdle_EnterCheck();
-        if (_BNormal_EnterCheck == true)
+        BIdle_CheckSwitch();
+    }
+
+    private void BNormal_CheckConditions()
+    {
+        BNormal_CheckSwitch();
+        BNormal_CheckSpawn();
+    }
+
+// --
+
+    private void BIdle_CheckSwitch()
+    {
+        if (entityStateDict.ContainsKey(MoveStateFactory.MoveStates.BNormal_MoveState) == true) // Check BNormal Switch
         {
-            movement_StateMachine.ChangeState(entityStateDict[MoveStateFactory.MoveStates.BNormal_MoveState]);
-            return;
+            var _BNormal_EnterCheck = BNormal_EnterCheck();
+            if (_BNormal_EnterCheck == true)
+            {
+                movement_StateMachine.ChangeState(entityStateDict[MoveStateFactory.MoveStates.BNormal_MoveState]);
+                return;
+            }
         }
     }
 
-    private void BNormal_Check()
+    private void BIdle_CheckSpawn() // Currently not called.
     {
-        var _BIdle_EnterCheck = BIdle_EnterCheck();
-        if (_BIdle_EnterCheck == true)
+        if (entityStateDict.ContainsKey(MoveStateFactory.MoveStates.BNormal_MoveState) == false) // Check BNormal Spawn (Currently empty)
         {
-            movement_StateMachine.ChangeState(entityStateDict[MoveStateFactory.MoveStates.BIdle_MoveState]);
-            return;
+            //
         }
     }
+
+// --
+
+    private void BNormal_CheckSwitch()
+    {
+        if (entityStateDict.ContainsKey(MoveStateFactory.MoveStates.BIdle_MoveState) == true) // Check BIdle Switch
+        {
+            var _BIdle_EnterCheck = BIdle_EnterCheck();
+            if (_BIdle_EnterCheck == true)
+            {
+                movement_StateMachine.ChangeState(entityStateDict[MoveStateFactory.MoveStates.BIdle_MoveState]);
+                return;
+            }
+        }
+    }
+
+    private void BNormal_CheckSpawn()
+    {
+        if (entityStateDict.ContainsKey(MoveStateFactory.MoveStates.BIdle_MoveState) == false) // Check BIdle Spawn
+        {
+            var statusBIdle = BIdle_SpawnNewCheck();
+            if (statusBIdle == true)
+            {
+                moveStateFactory.SpawnMoveState(MoveStateFactory.MoveStates.BIdle_MoveState, allConditionsDict, currentConditionsDict);
+                return;
+            }
+        }
+    }
+
+// --
+
 
     private bool BIdle_EnterCheck()
     {
-        if (Input.IsActionJustPressed("moveState_walk"))
+        if (Input.IsActionJustPressed("switchIdle"))
         {
             return true;
         }
         return false;
     }
 
+    private bool BIdle_SpawnNewCheck()
+    {
+        if (Input.IsActionJustPressed("spawnIdle"))
+        {
+            return true;
+        }
+        return false;
+    }
+
+// --
+
     private bool BNormal_EnterCheck()
     {
-        if (Input.IsActionJustPressed("moveState_run"))
+        if (Input.IsActionJustPressed("switchNormal"))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private bool BNormal_SpawnNewCheck()
+    {
+        if (Input.IsActionJustPressed("spawnNormal"))
         {
             return true;
         }
